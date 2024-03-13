e7afe77e8013ac5c14484a7339b6043e852b3679
vinit <vinit.londhe21@gmail.com>
2024-03-13 20:56:26 +0530

    model added

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from ChestDiseaseClassifier import logger
 from ChestDiseaseClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-#from ChestDiseaseClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from ChestDiseaseClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 #from KidneyDiseaseClassifier.pipeline.stage_03_model_training import ModelTrainingPipeline
 #from KidneyDiseaseClassifier.pipeline.stage_04_model_evaluation import EvaluationPipeline
 
@@ -11,6 +11,18 @@ try:
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Prepare Base Model"
+try:
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
     raise e
